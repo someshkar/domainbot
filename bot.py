6cdf4d0a00049466a117f4f36b823b0e69aa8e7f
Somesh Kar <somesh.kar@gmail.com>
2020-05-13 19:00:14 +0530

    Add featured domains

--- bot.py
+++ bot.py
@@ -3,9 +3,10 @@ import os
 import random
 import whois
 import json
+import validators
 from dotenv import load_dotenv
 
-from lib import domain_status
+from lib import domain_status, featured_domains
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -28,6 +29,15 @@ async def on_message(message):
 
     if message.content.startswith('domain'):
         domain = message.content.split(' ')[1]
+
+        if ' all ' in message.content:
+            domain = message.content.split(' ')[2]
+            status = featured_domains(domain)
+
+            print('"{}" returned "{}"'.format(domain, status))
+            await message.channel.send(status)
+            return
+
         status = domain_status(domain)
 
         print('"{}" returned "{}"'.format(domain, status))
--- lib.py
+++ lib.py
@@ -2,8 +2,8 @@ import json
 import whois
 import validators
 
-with open('./supported_tlds.json') as f:
-    supported_tlds = json.loads(f.read())
+with open('./tlds.json') as f:
+    tlds = json.loads(f.read())
 
 
 def domain_status(domain, expiry=True):
@@ -14,7 +14,7 @@ def domain_status(domain, expiry=True):
 
     tld = domain.split('.', 1)[1]
 
-    if tld not in supported_tlds:
+    if tld not in tlds['supported']:
         return "This TLD isn't currently supported!"
 
     domain_whois = whois.query(domain)
@@ -28,3 +28,32 @@ def domain_status(domain, expiry=True):
         args = [domain, domain_whois.registrar,
                 domain_whois.expiration_date.strftime("%B %d, %Y")]
         return '{} is registered at {} and will expire on {}.'.format(*args)
+
+
+def domain_registered(domain):
+    domain = domain.lower()
+    domain_whois = whois.query(domain)
+    if domain_whois is None:
+        return False
+    else:
+        return True
+
+
+def featured_domains(s):
+    s = s.lower()
+
+    available = []
+    message = ' may be available!'
+
+    for tld in tlds['featured']:
+        domain = s + '.' + tld
+        if not domain_registered(domain):
+            available.append(domain)
+
+    if len(available) > 1:
+        args = [', '.join(available[:-1]), available[-1]]
+        return '{} and {} may be available!'.format(*args)
+    elif len(available) == 1:
+        return '{} may be available!'.format(available[0])
+    else:
+        return 'None of the common TLDs are available for this.'
